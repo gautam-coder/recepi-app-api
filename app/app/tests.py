25a8f71f124fa7367917f1947f740e9e880755d1
Gautam Gaur <53681420+gautam-coder@users.noreply.github.com>
2022-12-21 17:32:14 +0530

    Add files via upload

---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+'''
+sample test 
+'''
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class Calc(SimpleTestCase):
+
+    def test_add_numbers(self):
+
+        res=calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+
